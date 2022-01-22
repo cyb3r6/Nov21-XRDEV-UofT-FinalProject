18c5512ed4efaa404f42eedcbd80113d13a7fd2e
Justin Clarke <ji_tlc@hotmail.com>
2022-01-22 13:05:41 -0500

    updates
    added new scene 'alpha1'

--- Nov21-UofT-Final Project/Assets/Team Alpha/Scripts/Timer.cs
+++ Nov21-UofT-Final Project/Assets/Team Alpha/Scripts/Timer.cs
@@ -1,9 +1,17 @@
+using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
+    public float? timer;
+
+    public bool toggle;
+
+    public float maxValue;
+    
+    public Text displayTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,20 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(toggle)
+        {
+            timer = 0f;
+            toggle = false;
+        }
+        if(timer.HasValue)
+        {
+            timer += Time.deltaTime;
+            displayTimer.text = timer.Value.ToString("F2");
+            if(timer.Value > maxValue)
+            {
+                timer = null;
+            }
+        }
     }
+    
 }
