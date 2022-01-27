c10bc7fc43d31eb13fa61eefa638016c2996611c
Jeremy K <73577116+JeremyKPark@users.noreply.github.com>
2022-01-27 17:15:56 -0500

    Added Rainbow Pillar on win + Fireworks

--- Nov21-UofT-Final Project/Assets/Team Nakisa4prez/Scripts/puzzle/CircuitSpawner.cs
+++ Nov21-UofT-Final Project/Assets/Team Nakisa4prez/Scripts/puzzle/CircuitSpawner.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class CircuitSpawner : MonoBehaviour
 {
     public GameObject correctPrefab;
+    public GameObject correctPrefabPillar;
+    public GameObject correctPrefabFireworks;
     public GameObject incorrectPrefab;
-    //[SerializeField]
-    //public GameManager gameManager;
-     
+    //private GameManager gameManager;
+      
     public void OnCircuit(CircuitContent.Circuit circuit)
     {
         correctPrefab = GameObject.Find("AlbertPrefab");
+        correctPrefabPillar = GameObject.Find("PillarPrefab");
         incorrectPrefab = GameObject.Find("LooseCube");
+        correctPrefabFireworks = GameObject.Find("Fireworks");
 
         if (circuit != null)
         {
-            Debug.Log("albert");
-            // gameManager.stoptimer = true;
-            Instantiate(correctPrefab, new Vector3(0, 5, -80), Quaternion.identity);
+            Debug.Log("albert ftw");
+            //gameManager.stoptimer = true;
+            Instantiate(correctPrefab, new Vector3(0, 10, -79), Quaternion.identity);
+            Instantiate(correctPrefabPillar, new Vector3(0, 3.5f, -171), Quaternion.identity);
+            Instantiate(correctPrefabFireworks, new Vector3(0, 10, -171), Quaternion.identity);
+            Instantiate(correctPrefabFireworks, new Vector3(5, 10, -171), Quaternion.identity);
+            Instantiate(correctPrefabFireworks, new Vector3(-10, 10, -171), Quaternion.identity);
         }
         else
         {
