8b6b46342a349523acf526ebb677230866b83ec9
Ken Bernardo <psykhaotic@gmail.com>
2022-01-27 13:53:38 -0500

    update

---
+++ Nov21-UofT-Final Project/Assets/Team Gamma/Scripts/PlayTone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTone : MonoBehaviour
+{
+    public AudioClip tone;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();   
+    }
+
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(tone);
+    }
+}
--- Nov21-UofT-Final Project/Assets/Team Gamma/Scripts/StayOnPedestal.cs
+++ Nov21-UofT-Final Project/Assets/Team Gamma/Scripts/StayOnPedestal.cs
@@ -3,16 +3,90 @@ using System.Collections;
  
 public class StayOnPedestal : MonoBehaviour
 {
-    public bool Sphere1Correct = false;
+    /// 1: Blue
+    //  2: Green 
+    //  3: Yellow 
+    //  4: Purple 
+    //  5: Red
+
+    public bool sphere1Correct = false;
+    public int correctSphereValue;
+    private int insertedSphereValue;
+    private bool occupied = false;
+
+    public BoxCollider dropZone;
   
     public void OnTriggerStay(Collider sphere) 
     {
-         if (sphere.gameObject.tag == "BlueSphere")
-         {
-            Sphere1Correct = true;
-            Debug.Log("Correct!");
-            Debug.Log(Sphere1Correct);
-         }
+        float speed = 0.1f;
+        float step = speed * Time.deltaTime;
+        Vector3 newSpherePosition = sphere.transform.position;
+
+        sphere.GetComponent<Rigidbody>().useGravity = false;
+        newSpherePosition = getNewSpherePosition();
+        sphere.transform.position = Vector3.MoveTowards(sphere.transform.position, newSpherePosition, step);
+
+        if (sphere.gameObject.tag == "SphereBlue")
+        {
+            insertedSphereValue = 1;
+        }
+        else if (sphere.gameObject.tag == "SphereGreen")
+        {
+            insertedSphereValue = 2;
+        }
+        else if (sphere.gameObject.tag == "SphereYellow")
+        {
+            insertedSphereValue = 3;
+        }
+        else if (sphere.gameObject.tag == "SpherePurple")
+        {
+            insertedSphereValue = 4;
+        }
+        else if (sphere.gameObject.tag == "SphereRed")
+        {
+            insertedSphereValue = 5;
+        }
+
+        if (correctSphereValue == insertedSphereValue)
+        {
+            sphere1Correct = true;
+            Debug.Log("Correct sphere placed!");
+
+            switch(insertedSphereValue)
+            {
+                case 1:
+                    Debug.Log("Blue sphere placed");
+                    break;
+                case 2:
+                    Debug.Log("Green sphere placed");
+                    break;
+                case 3:
+                    Debug.Log("Yellow sphere placed");
+                    break;
+                case 4:
+                    Debug.Log("Purple sphere placed");
+                    break;
+                case 5:
+                    Debug.Log("Red sphere placed");
+                    break;
+                default: 
+                    break;
+            }
+
+        }
     }
 
+    public void OnTriggerExit(Collider sphere) 
+    {
+        sphere.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private Vector3 getNewSpherePosition()
+    {
+        float x = dropZone.bounds.min.x + ((dropZone.bounds.max.x - dropZone.bounds.min.x) / 2);
+        float y = dropZone.bounds.min.y + ((dropZone.bounds.max.y - dropZone.bounds.min.y) / 2);
+        float z = dropZone.bounds.min.z + ((dropZone.bounds.max.z - dropZone.bounds.min.z) / 2);
+
+        return new Vector3(x, y, z);
+    }
 }
