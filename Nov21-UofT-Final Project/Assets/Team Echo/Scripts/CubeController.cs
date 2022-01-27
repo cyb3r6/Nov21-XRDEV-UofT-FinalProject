e5f0ffeb507351f33f6eee2b395b2e401c3058c1
Eduard Chiticari <58238391+EddyKudo@users.noreply.github.com>
2022-01-26 20:38:28 -0500

    Pyramid
    added rigid body to pyramid torus

---
+++ Nov21-UofT-Final Project/Assets/Team Echo/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Collider collider;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+
+        rigidbody.useGravity = false;
+        //rigidbody.isKinematic = true;
+        collider.enabled = true;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rigidbody.isKinematic = false;
+        rigidbody.useGravity = true;
+        collider.enabled = false;
+    }
+}
