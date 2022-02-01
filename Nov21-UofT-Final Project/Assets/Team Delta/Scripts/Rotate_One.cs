998da33771b2d64da2d5ce3d2bcf62b52631739a
RafaPay <bisara230@gmail.com>
2022-02-01 17:53:59 -0500

    Rebuild_model
    After problem loading to Github - re-build the clock

---
+++ Nov21-UofT-Final Project/Assets/Team Delta/Scripts/Rotate_One.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_One : MonoBehaviour
+{
+    //public float 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * -30); 
+    }
+}
